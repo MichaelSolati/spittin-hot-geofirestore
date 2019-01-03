6a9c3482f8437f594c78743eb4fd96aee777a63b
Michael Solati <mkslt04@gmail.com>
2019-01-02 17:03:12 -0800

    chore: update geofirestore to version 3.0.0

--- src/app/services/restaurants.service.ts
+++ src/app/services/restaurants.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
-import { GeoFirestore, GeoFirestoreQuery } from 'geofirestore';
+import { GeoFirestore, GeoCollectionReference, GeoQuery } from 'geofirestore';
 import { Geokit, LatLngLiteral } from 'geokit';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
@@ -9,19 +9,16 @@ import { LocationService } from './location.service';
 
 @Injectable()
 export class RestaurantsService {
-  private _geoFirestore: GeoFirestore = new GeoFirestore(firebase.firestore().collection('restaurants'));
+  private _collection: GeoCollectionReference = new GeoFirestore(firebase.firestore()).collection('restaurants');
   private _previousCoords: firebase.firestore.GeoPoint = new firebase.firestore.GeoPoint(0, 0);
   private _restaurants: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
-  private _query: GeoFirestoreQuery;
 
   constructor(private _ls: LocationService) {
-    this._createQuery();
-
     this._ls.mapCenter.subscribe((center: firebase.firestore.GeoPoint) => {
       if (Geokit.distance(this._geopoint2Literal(center), this._geopoint2Literal(this._previousCoords)) > 0.5) {
         this._previousCoords = center;
-        console.log('Updating GeoFirestore Query Center');
-        this._query.updateCriteria({ center });
+        console.log('Updating Center Of Query');
+        this._query(center);
       }
     });
   }
@@ -30,24 +27,15 @@ export class RestaurantsService {
     return this._restaurants.asObservable();
   }
 
-  private _createQuery(): void {
-    this._query = this._geoFirestore.query({
-      center: new firebase.firestore.GeoPoint(0, 0),
-      radius: .5
-    });
+  private _query(center = this._previousCoords): void {
+    const query = this._collection.near({ center, radius: .5 });
 
-    this._query.on('ready', () => console.log('GeoFirestore Query Complete'));
-
-    this._query.on('key_exited', ($key: string) => {
-      const places: any[] = [...this._restaurants.value];
-      this._restaurants.next(places.filter((place) => place.$key !== $key));
-    });
-
-    this._query.on('key_entered', ($key: string, result: any) => {
-      const places: any[] = [...this._restaurants.value];
-      result.$key = $key;
-      places.push(result);
-      this._restaurants.next(places);
+    query.get().then((snapshot) => {
+      console.log('New Snapshot');
+      const docs = snapshot.docs.map((doc) => {
+        return { $key: doc.id, ...doc.data() };
+      });
+      this._restaurants.next(docs);
     });
   }
 
